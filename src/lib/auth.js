0895ae4480260173b66977facc204189619f38b0
Mohim129 <countrysidefarm15@gmail.com>
2026-05-14 20:58:34 +0600

    signup and  signin done

---
+++ src/lib/auth.js
@@ -0,0 +1,15 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "@better-auth/mongo-adapter";
+
+const client = new MongoClient(process.env.TILES_DB_URI);
+
+const db = client.db('tiles-gallery-auth-db');
+export const auth = betterAuth({
+    emailAndPassword: { 
+    enabled: true, 
+  }, 
+  database: mongodbAdapter(db, {
+    client,
+  }),
+});
