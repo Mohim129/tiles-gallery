2789dad276a08dfe603a9ef3ee3ba6e5f27aa342
Mohim129 <countrysidefarm15@gmail.com>
2026-05-14 22:26:52 +0600

    testing deployment

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,7 +2,8 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "@better-auth/mongo-adapter";
 
-const client = new MongoClient(process.env.TILES_DB_URI);
+const uri = process.env.TILES_DB_URI || "mongodb://127.0.0.1:27017/fallback";
+const client = new MongoClient(uri);
 
 const db = client.db('tiles-gallery-auth-db');
 export const auth = betterAuth({
